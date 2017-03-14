c47c38f5f4af8416f3c2663c234c65cb22e96174
Elepert <emily.lepert@students.olin.edu>
2017-03-13 20:46:20 -0700

    Finished functions

--- frequency.py
+++ frequency.py
@@ -10,7 +10,34 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    curr_line = 0
+    while lines[curr_line].find('START OF THE PROJECT GUTENBERG EBOOK') == -1:
+    	curr_line += 1
+    lines = lines[curr_line+1:]
+
+    words = []
+
+    for i in lines:
+    	singular_word = ''
+    	j = 0
+    	i = i.lower()
+    	while j < len(i):
+    		if i[j] in string.punctuation:
+    			i = i.replace(i[j]," ")
+
+    		if i[j] not in string.whitespace:
+    			singular_word += i[j]
+
+    		if (j+1 < len(i)) and i[j+1] in string.whitespace:
+    			if singular_word != '':
+    				words.append(singular_word)
+    			j +=2
+    			singular_word = ''
+    		else:
+    			j += 1
+    return(words)
 
 
 def get_top_n_words(word_list, n):
@@ -23,8 +50,16 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    word_dict = {}
+    for word in word_list:
+    	if word in word_dict:
+    		word_dict[word] += 1
+    	else:
+    		word_dict[word] = 1
+
+    word_dict = sorted(word_dict, key = word_dict.get, reverse = True)
+    return(word_dict[:n])
+
 
 if __name__ == "__main__":
-    print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    print(get_top_n_words(get_word_list('WutheringHeights.txt'), 100))
